e4f98b466fb4fa571e6926823826a01fdd8767f6
Will <willstruder@gmail.com>
2021-06-25 01:04:53 -0400

    Refactored HTML/CSS/JS files

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ var dayDisplay = $('#current-Day');
 dayDisplay.text(moment().format('dddd, MMMM Do, YYYY'));
 
 
+var plannerText = $("textarea");
+var saveFile = $(".saveBtn");
+
+function storeLocal(div){
+    localStorage.getItem('textarea', []); 
+    document.getElementById('') 
+}
+
+$('textarea').on('click', function(event){
+    var div = event.target;
+    if (div.matches('.saveBtn')) {
+        storeLocal(div);
+    }
+});
+
+
