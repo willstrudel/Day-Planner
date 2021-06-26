58261f7ec01522ec11a8450fab3ae71dd8e5b5bd
Will <willstruder@gmail.com>
2021-06-25 23:46:47 -0400

    Applied color styling to time blocks

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
 
-console.log("script imported");
 
 var dayDisplay = $('#current-Day'); 
 dayDisplay.text(moment().format('dddd, MMMM Do, YYYY'));
 
 
-var plannerText = $("textarea");
-var saveFile = $(".saveBtn");
-
-
 function storeLocal(){
     console.log("I'm here");
 
-    localStorage.getItem('textarea', JSON.stringify(data)); 
-    document.getElementById('') 
+    localStorage.getItem('textarea').value; 
+    document.getElementById('.saveBtn');
 }
 
 
